1a7a37fda4f7c513aa41f0595ba7aa0372af08a7
ketan1324 <ketanmaheshdoshi@gmail.com>
2024-03-25 06:42:10 +0530

    initial message

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css"
+export default function App({ Component, pageProps }) {
+  return (
+    <h1>Hello</h1>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
+
+import './_app'
+
+
+export default function Home() {
+  return (
+   <_app></_app>
+  );
+}
